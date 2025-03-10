41798b5580a93148c9e5676feeee1325ee18aa5f
Hamadullah12 <hamadfms05@gmail.com>
2025-03-10 20:04:50 +0800

    requiting from other directoreis

---
+++ math.js
@@ -0,0 +1,28 @@
+exports.add = (a, b) => {
+  return a + b;
+};
+exports.mult = (a, b) => {
+  return a * b;
+};
+exports.square = (a, b) => {
+  return a ** b;
+};
+exports.g = 9.8;
+// module.exports.add = (a, b) => {
+//   return a + b;
+// };
+// module.exports.mult = (a, b) => {
+//   return a * b;
+// };
+// module.exports.square = (a, b) => {
+//   return a ** b;
+// };
+// module.exports.g = 9.8;
+
+// let obj = {
+//   add: "add",
+//   mult: "multi",
+//   square: "square",
+//   g: "g",
+// };
+// module.exports = obj;
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let args = process.argv;
+// let args = process.argv;
+// for (let i = 2; i < args.length; i++) {
+//   console.log("hello dear ", args[i]);
+// }
+// modules
+// const maths = require("./math");
+// console.log(maths.mult(2, 6));
 
-for (let i = 2; i < args.length; i++) {
-  console.log("hello dear ", args[i]);
-}
+// requiring form other directoreis
+
+const fruitData = require("./Fruit");
+console.log(fruitData[0]);
